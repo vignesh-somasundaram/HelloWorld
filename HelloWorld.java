b93d07ec12419d8707a9f1c338e15ad9df5e56b5
vignesh-somasundaram <80034891+vignesh-somasundaram@users.noreply.github.com>
2021-03-11 15:23:44 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World from Vignesh Somasundaram  :)"); 
+        System.out.println("Hello World from Vignesh Somasundaram  :) Have a good day!"); 
     }
 }
